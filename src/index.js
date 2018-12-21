5aeddb2bc55f23163680f71ce5945ec56c64a411
Eric Henderson <henderea@gmail.com>
2018-12-21 13:58:31 -0500

    some cleanup and refactoring

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-var sass = require('sass');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+const sass = require('sass');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Handler = require('../lib/handler');
 
-const jsonBody = bodyParser.json(); // for parsing application/json
-const urlBody = bodyParser.urlencoded({ extended: true }); // for parsing application/x-www-form-urlencoded
-const main = (req, res) => () => {
+const handler = new Handler();
+handler.use(cors());
+handler.use(bodyParser.json()); // for parsing application/json
+handler.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+handler.handle((req, res) => {
     const input = req.body.sass;
-    res.setHeader('Content-Type',  'text/css; charset=utf-8');
-    res.end(sass.renderSync({data: input}).css);
-}
-const body = (req, res) => () => jsonBody(req, res, () => urlBody(req, res, main(req, res)));
-const corsHandler = (req, res) => () => cors()(req, res, body);
+    res.setHeader('Content-Type', 'text/css; charset=utf-8');
+    res.end(sass.renderSync({ data: input }).css);
+});
 
-module.exports = (req, res) => corsHandler(req, res)();
+module.exports = handler.handler;
